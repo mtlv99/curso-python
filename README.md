1889e7fbb7f762401908cef31fbabeafcfe3d3ca
Marco León <marco.tulio.leon@gmail.com>
2024-01-16 21:29:47 -0600

    Section 2: lesson 13 - data types

---
+++ README.md
@@ -0,0 +1,13 @@
+# Curso Python
+
+Estoy siguiendo este curso de Python / I'm following this Python Course:
+
+https://www.udemy.com/course/python-total/
+
+English warning: For this repo, I wrote all the notes in Spanish, this is a personal project where I take notes for this Python course.
+
+
+### Nota
+Repo para tomar notas acerca de Python, uso bastante Spanglish. Además, es bastante probable que este repo no esté muy limpio, lo uso para tomar apuntes que me sirven para otros proyectos.
+
+
---
+++ day-01-helloworld/Primer_Proyecto.py
@@ -0,0 +1,22 @@
+# print("Holaaa")
+
+# input se queda esperando por la respuesta
+# print("Tu nombre es: " + input("Escribe tu nombre: "))
+# print("Tu apellido es: " + input("Escribe tu apellido: "))
+
+# se pueden tener multiples input en el mismo string
+# print("Tu nombre es: " + input("Escribe tu nombre: ") + " " + input("Escribe tu apellido: "))
+
+
+# Proyecto
+print("La respuesta es\n\"" + input("En cual planeta vives?: ") + " " + input("Cual es tu color favorito?: ") + "\"")
+
+# Tipos de datos
+# string (str)
+# int (int)
+# floating (float)
+# listas (lst) - diferentes tipos de datos, es indexable
+# diccionarios (dic) - valor key/value - no ordenados
+# tuples (tup) - parecidos a listas - no pueden modificar su orden
+# sets (set) - conjunto de elementos unicos - no se pueden repetir.
+# booleanos (bool)
